4b523036d8e905cca362af64074e00370e34bec1
ncrecc <ncrecc@gmail.com>
2020-04-25 17:16:12 -0400

    getting stuff in here

---
+++ README.md
@@ -0,0 +1,1 @@
+More Swaps is a collab mod for Dicey Dungeons that adds more equipment swapping rules
---
+++ data/text/generators/remixrules.hx
@@ -0,0 +1,9 @@
+Remix.reset();
+Remix.mode = "challenge";
+Remix.settotalremixes(3);
+
+Remix.offer(2, '', ['Rover Swap']);
+Remix.offer(3, '', ['Wisp Swap']);
+Remix.offer(4, '', ['Gargoyle Swap']);
+
+trace("THIS IS A TEST! not reflective of how final moreswaps should work");
