4f25cd3a2298b9efe9f045976c32611d48b82d96
ncrecc <ncrecc@gmail.com>
2020-05-23 02:38:53 -0400

    thing

--- data/text/generators/remixrules.hx
+++ data/text/generators/remixrules.hx
@@ -4,7 +4,7 @@ Remix.settotalremixes(3);
 
 Remix.offer(2, '', [pick(['Liquid Nitrogen Canister', 'Sludge Swap'])]);
 Remix.offer(3, '', ['Empowered Wisp']);
-Remix.offer(4, '', ['Hat o' Rejects']);
+Remix.offer(4, '', ["Hat o' Rejects"]);
 Remix.offer(5, '', ['Rover']);
 
 trace("THIS IS (STILL) A TEST! not reflective of how final moreswaps should work");
